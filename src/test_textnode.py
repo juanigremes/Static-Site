ea1cda35e07d21f961bf47cbb220a5b7ff3ab7b3
juanigremes <juanigremes@gmail.com>
2026-04-26 21:08:42 -0300

    comienzo de tests y textnode

---
+++ src/test_textnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    # HACER MAS TESTS!!
+
+
+if __name__ == "__main__":
+    unittest.main()
