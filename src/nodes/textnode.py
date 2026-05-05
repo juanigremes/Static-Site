c7b8660d31dc7fe7f8b853247164d7175d5a5a85
juanigremes <juanigremes@gmail.com>
2026-05-05 11:48:25 -0300

    markdown blocks

--- src/nodes/textnode.py
+++ src/nodes/textnode.py
@@ -150,3 +150,118 @@ def text_to_textnodes(text):
     nodes = split_nodes_image(nodes)
     nodes = split_nodes_link(nodes)
     return nodes
+
+
+
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    res = []
+    for block in blocks:
+        if block != "":
+            res.append(block.strip())
+    return res
+
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+
+def block_to_block_type(block):
+    res = BlockType.PARAGRAPH
+    
+    if is_heading(block):
+        res = BlockType.HEADING
+    
+    elif is_code(block):
+        res = BlockType.CODE
+    
+    elif is_quote(block):
+        res = BlockType.QUOTE
+    
+    elif is_unordered_list(block):
+        res = BlockType.UNORDERED_LIST
+    
+    elif is_ordered_list(block):
+        res = BlockType.ORDERED_LIST
+    
+    return res
+
+def is_heading(block):
+    cant_numerales = 0
+    block_len = len(block)
+    for i in range(block_len):
+        char = block[i]
+        if char == '#':
+            cant_numerales += 1
+            if cant_numerales > 6:
+                return False
+        else:
+            break
+    if char != " ":
+        return False
+    if i == block_len:
+        return False
+    return True
+
+def is_code(block):
+    cant_backticks = 0
+    block_len = len(block)
+    if block_len <= 6:
+        return False
+    for i in range(block_len):
+        char = block[i]
+        if char =='`':
+            cant_backticks += 1
+            if cant_backticks > 3:
+                return False
+        else:
+            break
+    if cant_backticks != 3 or char != '\n':
+        return False
+    for j in range(1,4):
+        if block[block_len-j] == '`':
+            cant_backticks -= 1
+    if cant_backticks != 0:
+        return False
+    return True
+
+def is_quote(block):
+    if block[0] == '>':
+        return True
+    return False
+
+def is_unordered_list(block):
+    lines = block.split('\n')
+    for line in lines:
+        if line == '':
+            continue
+        if len(line) < 2:
+            return False
+        if line[0] != '-':
+            return False
+        if line[1] != ' ':
+            return False
+    return True
+
+def is_ordered_list(block):
+    lines = block.split('\n')
+    index = 1
+    for line in lines:
+        if line == '':
+            continue
+        if len(line) < 3:
+            return False
+        if line[0] != f"{index}":
+            return False
+        if line[1] != '.':
+            return False
+        if line[2] != ' ':
+            return False
+        index += 1
+    return True
--- src/tests/test_textnode.py
+++ src/tests/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from src.nodes.textnode import TextNode, TextType, text_node_to_html_node, split_nodes_delimeter, extract_markdown_images, extract_markdown_links, split_nodes_link, split_nodes_image, text_to_textnodes
+from src.nodes.textnode import *#TextNode, TextType, text_node_to_html_node, split_nodes_delimeter, extract_markdown_images, extract_markdown_links, split_nodes_link, split_nodes_image, text_to_textnodes, markdown_to_blocks
 
 
 class TestTextNode(unittest.TestCase):
@@ -243,6 +243,174 @@ class TestTextToTextNodes(unittest.TestCase):
         self.assertEqual(new_nodes, [TextNode("this is text with ", TextType.TEXT), TextNode("italic", TextType.ITALIC)])   
     
 
+class TestMarkdownToBlocks(unittest.TestCase):
+        
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
+
+    def test_markdown_to_blocks_01(self):
+        md = """
+This is __italic__ paragraph
+
+
+
+
+This is another paragraph with **bold** text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+
+
+
+- this is another list
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is __italic__ paragraph",
+                "This is another paragraph with **bold** text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list",
+                "- this is another list",
+            ],
+        )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+
+    def test_paragraph(self):
+        block = "this is just paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    
+    
+
+    
+    def test_heading_1(self):
+        block = "# this is heading 1"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_heading_2(self):
+        block = "## this is heading 2"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_heading_3(self):
+        block = "### this is heading 3"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_heading_4(self):
+        block = "#### this is heading 4"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_heading_5(self):
+        block = "##### this is heading 5"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_heading_6(self):
+        block = "###### this is heading 6"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_paragraph_not_heading_1(self):
+        block = "####### this is just paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_paragraph_not_heading_2(self):
+        block = "###this is just paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    
+
+    
+    def test_code(self):
+        block = """```
+this is multiline code
+```"""
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+ 
+    def test_code_1(self):
+        block = """```
+```"""
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+ 
+    def test_paragraph_not_code_1(self):
+        block = "```this is just paragraph```"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+ 
+    def test_paragraph_not_code_2(self):
+        block = """```
+this is just paragraph"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+ 
+    def test_paragraph_not_code_3(self):
+        block = """``
+this is just paragraph```"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    
+
+    
+    def test_quote1(self):
+        block = ">this is a quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+ 
+    def test_quote2(self):
+        block = "> this is a quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    def test_quote_3(self):
+        block = ">"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+ 
+
+
+    
+    def test_unordered_list_1(self):
+        block = """- this
+- is
+- unordered
+- list"""
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+ 
+    def test_paragraph_not_unordered_list_1(self):
+        block = "-there is no spacte after the \"-\""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+ 
+
+
+
+    def test_ordered_list_1(self):
+        block = """1. this
+2. is
+3. ordered
+4. list"""
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+ 
+    def test_paragraph_not_ordered_list_1(self):
+        block = """2. this is just paragraph
+3. because it does not have a 1"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+ 
+    def test_paragraph_not_ordered_list_2(self):
+        block = "1.this is just paragraph beacause there is no space after the number"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+ 
+    
 
 
 
