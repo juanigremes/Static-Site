3edd36f0a310d92f41b7fab4f97d997f1b270984
juanigremes <juanigremes@gmail.com>
2026-05-08 12:29:14 -0300

    copy static to public

--- src/main.py
+++ src/main.py
@@ -1,13 +1,42 @@
-from textnode import *
-
+import os
+import shutil
 
 def main():
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
+    copy_from_to("static", "public")
 
+def copy_from_to(source, destination):
+    if os.path.exists(source):
+        delete_all_directory(destination)
+        copy_all_directory(source, destination, 0)
+    else:
+        raise Exception(f"source \"{source}\" does not exist")
 
+def delete_all_directory(directory):
+    print(f"deleting the destination directory: {directory}")
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    print("successfully deleted \n")
+    return
 
+def copy_all_directory(source, destination, depth):
+    tabs = "  " * depth
+    print(f"{tabs}copying from \"{source}\" to \"{destination}\"")
+    os.mkdir(destination)
 
+    source_content = os.listdir(source)
+
+    for item in source_content:
+        item_path = os.path.join(source, item)
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, destination)
+            print(f"{tabs}succsessfully copied {item} to {destination}")
+        else:
+            print(f"\n{tabs}  recursion  ---------------------------------")
+            new_dest = os.path.join(destination, item)
+            copy_all_directory(item_path, new_dest, depth+1)
+
+    print(f"{tabs}end recursion ----------------------------------\n")
+    return
 
 
 main()
